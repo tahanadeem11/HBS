9f288d6607891b0825348f739f0a98d967e800d0
Taha_Nadeem <tahanadeem.gk@gmail.com>
2025-12-20 02:36:47 +0500

    Completed fianl

--- assets/js/header-data.js
+++ assets/js/header-data.js
@@ -62,7 +62,7 @@ const headerHTML = `
         <div class="contact-info">
             <h4>Contact Info</h4>
             <ul>
-                <li><span class="fa fa-phone"></span> <a href="tel:9253397692">(925) 339-7692</a></li>
+                <li><span class="fa fa-phone"></span> <a href="tel:+19255239723">(925)-523-9723</a></li>
                 <li><span class="fa fa-envelope"></span> <a href="mailto:sohny@homebysohny.com">sohny@homebysohny.com</a></li>
             </ul>
         </div>
@@ -107,7 +107,7 @@ const headerHTML = `
                     <div class="argo-info-icon"><span class="flaticon-telephone"></span></div>
                     <div>
                         <strong>Office Number</strong>
-                        <span><a href="tel:9253397692">(925) 339-7692</a></span>
+                        <span><a href="tel:+19255239723">925-523-9723</a></span>
                     </div>
                 </div>
                 <div class="argo-info-item">
